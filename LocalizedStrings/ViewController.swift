36a53209f6dc07144297162210400aa967669588
Vadim Shpakovski <vadim@shpakovski.com>
2015-02-20 16:10:14 +0300

    Initial Commit

---
+++ LocalizedStrings/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LocalizedStrings
+//
+//  Created by Vadim Shpakovski on 2/20/15.
+//  Copyright (c) 2015 Vadim Shpakovski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
