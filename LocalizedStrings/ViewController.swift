2fa2932fe9d34d5895a9e044e5317766b0d7e73d
Vadim Shpakovski <vadim@shpakovski.com>
2015-02-22 13:57:25 +0300

    Fixes variable row heights and uses cache.

--- LocalizedStrings/ViewController.swift
+++ LocalizedStrings/ViewController.swift
@@ -33,22 +33,40 @@ class ViewController: NSViewController {
     }
     
     @IBOutlet var arrayController: NSArrayController?
+    
+    var cellSizeCache = [Int: CGSize]()
+    var minCellSizeHeight = CGFloat.max
 }
 
 extension ViewController: NSTableViewDelegate {
     
     func tableView(tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
         
+        if let cachedSize = self.cellSizeCache[row] {
+            return cachedSize.height
+        }
+        
         if let tableColumn = tableView.tableColumns.first as? NSTableColumn {
             if let cellView = tableView.makeViewWithIdentifier(tableColumn.identifier, owner: self) as? NSTableCellView {
-                if let objectValues = self.arrayController?.content as? [LocalizedString] {
-                
-                    cellView.objectValue = objectValues[row]
-                    cellView.needsLayout = true
-                    cellView.layoutSubtreeIfNeeded()
-                    
-                    let cellSize = cellView.fittingSize
-                    return cellSize.height
+                if let textField = cellView.textField {
+                    if let objectValues = self.arrayController?.content as? [LocalizedString] {
+
+                        let cellRect = cellView.frame
+                        self.minCellSizeHeight = min(self.minCellSizeHeight, CGRectGetHeight(cellRect))
+                        
+                        let nibTextWidth = CGRectGetWidth(textField.frame)
+                        let nibTextInset = CGRectGetWidth(cellRect) - nibTextWidth
+                        let preferredTextWidth = tableColumn.width - nibTextInset
+                        textField.preferredMaxLayoutWidth = preferredTextWidth
+                        
+                        cellView.objectValue = objectValues[row]
+                        cellView.needsLayout = true
+                        cellView.layoutSubtreeIfNeeded()
+                        
+                        let cellSize = cellView.fittingSize
+                        self.cellSizeCache[row] = cellSize
+                        return cellSize.height
+                    }
                 }
             }
         }
@@ -57,14 +75,35 @@ extension ViewController: NSTableViewDelegate {
     
     func tableViewColumnDidResize(notification: NSNotification) {
         if let tableView = notification.object as? NSTableView {
-            if let bounds = tableView.enclosingScrollView?.contentView.bounds {
-                let visibleRows = tableView.rowsInRect(bounds)
-                let indexes = NSIndexSet(indexesInRange: visibleRows)
-                
-                NSAnimationContext.beginGrouping()
-                NSAnimationContext.currentContext().duration = 0.0
-                tableView.noteHeightOfRowsWithIndexesChanged(indexes)
-                NSAnimationContext.endGrouping()
+            if let newColumnWidth = (notification.userInfo?["NSTableColumn"] as? NSTableColumn)?.width {
+                if let oldColumnWidth = notification.userInfo?["NSOldWidth"] as? CGFloat {
+                    
+                    let invalidRows = NSMutableIndexSet()
+                    for (row, cellSize) in self.cellSizeCache {
+                        
+                        if newColumnWidth > oldColumnWidth { // |     -> |
+                            if cellSize.height > self.minCellSizeHeight { // multiline may fit into the new width
+                                invalidRows.addIndex(row)
+                            }
+                        }
+                        else { // |     | <-
+                            if newColumnWidth < cellSize.width { // column width is less than already taken space
+                                invalidRows.addIndex(row)
+                            }
+                        }
+                    }
+                    
+                    if invalidRows.count > 0 {
+                        invalidRows.enumerateIndexesUsingBlock { invalidRow, _ in
+                            self.cellSizeCache[invalidRow] = nil
+                        }
+                        
+                        NSAnimationContext.beginGrouping()
+                        NSAnimationContext.currentContext().duration = 0.0
+                        tableView.noteHeightOfRowsWithIndexesChanged(invalidRows)
+                        NSAnimationContext.endGrouping()
+                    }
+                }
             }
         }
     }
